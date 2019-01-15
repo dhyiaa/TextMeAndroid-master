a5fcb857050379e15cb9190bba7c90d12df26375
MalcolmWang999 <1391036978@qq.com>
2019-01-15 13:26:18 -0500

    No Message

--- app/src/main/java/com/link/dheyaa/textme/activities/MainActivity.java
+++ app/src/main/java/com/link/dheyaa/textme/activities/MainActivity.java
@@ -63,6 +63,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity   {
 
+    // variable declaration
     private FirebaseAuth mAuth;
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
@@ -73,83 +74,81 @@ public class MainActivity extends AppCompatActivity   {
     private ImageView profileView;
     public MainActivity _context;
 
-
-
+    /* onCreate method for activity
+     * @param savedInstanceState - data bundle for activity
+     * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
+        // set theme
         SharedPreferences prefs = getSharedPreferences("textMeSP", MODE_PRIVATE);
-
         String isDark = prefs.getString("isDark", null);
-        if (isDark != null) {
+        if (isDark != null) { // if user chooses dark mode
             setTheme (R.style.ActivityTheme_Primary_Base_Dark);
-        }else{
+        }else{ // if user does not choose dark mode
             setTheme (R.style.ActivityTheme_Primary_Base_Light);
         }
-        super.onCreate (savedInstanceState);
 
+        super.onCreate (savedInstanceState); // call super method
+
+        // database setup
         mAuth = FirebaseAuth.getInstance ();
         DBref = FirebaseDatabase.getInstance ().getReference ("Users");
 
-        updateUI (mAuth.getCurrentUser ());
-        showContent ();
+        updateUI (mAuth.getCurrentUser ()); // update UI with current user
+        showContent (); // show content
     }
 
-    public static int getAttributeColor(
-            Context context,
-            int attributeId) {
-        TypedValue typedValue = new TypedValue();
-        context.getTheme().resolveAttribute(attributeId, typedValue, true);
-        int colorRes = typedValue.resourceId;
-        int color = -1;
-        try {
-            color = context.getResources().getColor(colorRes);
-        } catch (Resources.NotFoundException e) {
-        }
-        return color;
-    }
-
+    /* method to show contents
+     * no params
+     * */
     private void showContent() {
+
         setContentView (R.layout.activity_main);
 
+        // find views
         _context = this;
         profileView = (ImageView) findViewById (R.id.profileView);
         toolBarTitle = (TextView) findViewById (R.id.toolBarTitle);
         toolbar = (Toolbar) findViewById (R.id.toolbar);
         setSupportActionBar (toolbar);
 
-
+        // create bottom navigation items
         final AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById (R.id.bottom_navigation);
-
         AHBottomNavigationItem item1 = new AHBottomNavigationItem (R.string.tab_text_1, R.drawable.friends, R.color.colorPrimaryText_dark);
         AHBottomNavigationItem item2 = new AHBottomNavigationItem (R.string.tab_text_3, R.drawable.requests, R.color.colorPrimary);
         AHBottomNavigationItem item3 = new AHBottomNavigationItem (R.string.tab_text_4, R.drawable.settings, R.color.colorPrimaryDark);
 
-        com.mikhaellopez.circularimageview.CircularImageView profile = findViewById (R.id.profileView) ;
+        com.mikhaellopez.circularimageview.CircularImageView profile = findViewById (R.id.profileView);
 
+        // add items to bottom navigation
         bottomNavigation.addItem (item1);
         bottomNavigation.addItem (item2);
         bottomNavigation.addItem (item3);
 
+        // set theme 1
         TypedValue typedValue = new TypedValue ();
         Resources.Theme theme = this.getTheme ();
         theme.resolveAttribute (R.attr.colorPrimary, typedValue, true);
         @ColorInt int color = typedValue.data;
 
-
+        // set theme 2
         TypedValue typedValue2 = new TypedValue ();
         Resources.Theme theme2 = this.getTheme ();
         theme.resolveAttribute (R.attr.colorPrimary, typedValue, true);
         @ColorInt int color2 = typedValue.data;
 
+        // set attributes of bottom navigation
         bottomNavigation.setDefaultBackgroundColor (color2);
         bottomNavigation.setBehaviorTranslationEnabled (false);
         bottomNavigation.setAccentColor ( Color.parseColor ("#FFFFFF"));
         bottomNavigation.setInactiveColor (Color.argb (100 , 255 ,255 ,255));
-
         bottomNavigation.setOnTabSelectedListener (new AHBottomNavigation.OnTabSelectedListener () {
+
+            /* method launches when tab is selected
+             * @param position - position of tab
+             * @param wasSelected - true if selected
+             * */
             @Override
             public boolean onTabSelected(int position, boolean wasSelected) {
                 mViewPager.setCurrentItem (position);
@@ -157,30 +156,48 @@ public class MainActivity extends AppCompatActivity   {
             }
         });
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter (getSupportFragmentManager ());
-
+        // set view pager
+        mSectionsPagerAdapter = new SectionsPagerAdapter (getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById (R.id.container);
         mViewPager.setAdapter (mSectionsPagerAdapter);
         mViewPager.addOnPageChangeListener (new ViewPager.OnPageChangeListener () {
+
+            /* method launches when page is scrolled
+             * @param position - position on page
+             * @param positionOffset - offset of position
+             * @param positionOffsetPixels - pixels of offset
+             * */
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            /* method launches when page is selected
+             * @param position - position on page
+             * */
             @Override
             public void onPageSelected(int position) {
-                bottomNavigation.setCurrentItem (position);
+                bottomNavigation.setCurrentItem (position); // set item by position
             }
+
+            /* method launches when scroll state of page changes
+             * @param state - state of scroll
+             * */
             @Override
-            public void onPageScrollStateChanged(int state) {
-            }
+            public void onPageScrollStateChanged(int state) {}
+
         });
 
         mViewPager.setOffscreenPageLimit (4);
 
-        getSupportActionBar ().setDisplayShowTitleEnabled (false);
+        getSupportActionBar ().setDisplayShowTitleEnabled (false); // disable display
 
-        profile.setOnClickListener (new View.OnClickListener () {
+        profile.setOnClickListener (new View.OnClickListener(){ // listen for clicking on profile image
+
+            /** method launches when profile image is clicked
+             * @param v - current view
+             */
             @Override
             public void onClick(View v) {
+                // go to settings page
                 mViewPager.setCurrentItem (2);
                 bottomNavigation.setCurrentItem (2);
             }
@@ -188,31 +205,43 @@ public class MainActivity extends AppCompatActivity   {
 
     }
 
-
+    // listen for changes of authentication state
     FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener () {
+
+        /* method launches when auth state changes
+         * @param firebaseAuth - auth of firebase
+         * */
         @Override
         public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
             FirebaseUser user = firebaseAuth.getCurrentUser ();
-            if (user == null) {
-                startActivity (new Intent (MainActivity.this, SignIn.class));
-                finish ();
+            if (user == null) { // if there is no user logged in
+                startActivity (new Intent (MainActivity.this, SignIn.class)); // start sign in activity
+                finish (); // terminate activity
             }
         }
     };
 
+    /* method to update UI
+     * @param currentUser - current user logged in
+     * */
     public void updateUI(FirebaseUser currentUser) {
-        if (currentUser == null) {
-            startActivity (new Intent (getApplicationContext (), SignIn.class));
+        if (currentUser == null) { // if there is no user logged in
+            startActivity (new Intent (getApplicationContext(), SignIn.class)); // start sign in activity
             try {
-                FirebaseInstanceId.getInstance().deleteInstanceId ();
-            } catch (IOException e) {
-                e.printStackTrace ();
+                FirebaseInstanceId.getInstance().deleteInstanceId (); // try to delete instance id
+            } catch (IOException e) { // catch error
+                e.printStackTrace (); // print error
             }
-            this.finish ();
+            this.finish (); // finish activity
 
-        } else {
+        } else { // if there is a user logged in
 
+            // listen for successful instance id result to firebase
             FirebaseInstanceId.getInstance().getInstanceId ().addOnSuccessListener (new OnSuccessListener<InstanceIdResult> () {
+
+                /* method launches when instance id result update is successful
+                 * @param instanceIdResult - result of instance id
+                 * */
                 @Override
                 public void onSuccess(InstanceIdResult instanceIdResult) {
                     String token = FirebaseInstanceId.getInstance ().getToken ();
