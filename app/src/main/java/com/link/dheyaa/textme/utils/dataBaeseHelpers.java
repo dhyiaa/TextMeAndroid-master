b7bd4abcdca5c513c6151c56d8fb7aa79e7bc279
dhyiaa <alialiali3957@gmail.com>
2019-01-15 13:34:41 -0500

    Merge branch 'master' of https://github.com/dhyiaa/TextMeAndroid-master
    # Conflicts:
    #	app/src/main/java/com/link/dheyaa/textme/activities/MainActivity.java

--- app/src/main/java/com/link/dheyaa/textme/utils/dataBaeseHelpers.java
+++ app/src/main/java/com/link/dheyaa/textme/utils/dataBaeseHelpers.java
@@ -1,13 +1,28 @@
 package com.link.dheyaa.textme.utils;
+/* TextMe Team
+ * Jan 2019
+ * dataBaeseHelpers class:
+ * this class is used to change the token from the database
+ *
+ * it is separated to we use it in different placed inside the project
+ */
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class dataBaeseHelpers {
-    public static void setToken(String token){
+    /**
+     * this function changing the token from database
+     *
+     * @param token : the token as String
+     */
+    public static void setToken(String token) {
+        // instantiating the FirebaseAuth class
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        // getting the database refrence to the users node
         DatabaseReference DBref = FirebaseDatabase.getInstance().getReference("Users");
+        // udpate the current user notification token
         DBref.child(mAuth.getUid()).child("registrationToken").setValue(token);
     }
 }
