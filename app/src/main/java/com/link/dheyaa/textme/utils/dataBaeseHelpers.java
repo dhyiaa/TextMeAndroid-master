1c0080b13637308b44420dc05247e4582362cad0
MalcolmWang999 <1391036978@qq.com>
2019-01-15 13:35:53 -0500

    No Message

--- app/src/main/java/com/link/dheyaa/textme/utils/dataBaeseHelpers.java
+++ app/src/main/java/com/link/dheyaa/textme/utils/dataBaeseHelpers.java
@@ -1,4 +1,4 @@
-package com.link.dheyaa.textme.utils;
+
 /* TextMe Team
  * Jan 2019
  * dataBaeseHelpers class:
@@ -6,6 +6,9 @@ package com.link.dheyaa.textme.utils;
  *
  * it is separated to we use it in different placed inside the project
  */
+
+
+package com.link.dheyaa.textme.utils;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -20,9 +23,9 @@ public class dataBaeseHelpers {
     public static void setToken(String token) {
         // instantiating the FirebaseAuth class
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        // getting the database refrence to the users node
+        // getting the database reference to the users node
         DatabaseReference DBref = FirebaseDatabase.getInstance().getReference("Users");
-        // udpate the current user notification token
+        // update the current user notification token
         DBref.child(mAuth.getUid()).child("registrationToken").setValue(token);
     }
 }
